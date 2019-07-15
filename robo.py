e55b98416f72986c893ba4762598aac138b289bf
ph1987 <ph1987@gmail.com>
2019-07-15 02:42:24 -0300

    melhorias: consegui puxar eventos após fazer login no fb

---
+++ robo.py
@@ -0,0 +1,22 @@
+# src: https://gist.github.com/UndergroundLabs/fad38205068ffb904685
+import requests
+
+USERNAME = 'dickvigarista888@gmail.com'
+PASSWORD = 'paocomovo1'
+PROTECTED_URL = 'https://m.facebook.com/barbukowskirio/events'
+
+def auth(session, email, password):
+    response = session.post('https://m.facebook.com/login.php', data={
+        'email': email,
+        'pass': password
+    }, allow_redirects=False)
+    return response.cookies
+
+def login():
+    session = requests.session()
+    cookies = auth(session, USERNAME, PASSWORD)
+    return cookies, session
+
+cookie, s = login()
+response = s.get(PROTECTED_URL, cookies=cookie, allow_redirects=False)
+print(response.text)
